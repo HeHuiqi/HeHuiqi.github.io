608dc8db2d87f04373fee400c709b1fa147900a9
HeHuiqi <1710308677@qq.com>
2022-08-17 16:06:17 +0800

    support sequence

--- js/content.js
+++ js/content.js
@@ -6,13 +6,18 @@ if (start == 0) {
    title = '基础语法.md'; 
 }
 
-mermaid.initialize({startOnLoad:true});
-
-
-const mermaidChart = (code) => {
+// mermaid.initialize({startOnLoad:true});
+const mermaidChart = (code_str) => {
   try {
-    mermaid.parse(code)
-    return `<div class="mermaid">${code}</div>`
+    mermaid.parse(code_str)
+    return `<div class="mermaid">${code_str}</div>`
+  } catch ({ str, hash }) {
+    return `<pre>${str}</pre>`
+  }
+};
+const sequenceChart = (code_str) => {
+  try {
+    return `<div class="diagram">${code_str}</div>`
   } catch ({ str, hash }) {
     return `<pre>${str}</pre>`
   }
@@ -24,22 +29,25 @@ const md = new markdownit({
     html: true,
     linkify: true,
     typographer: true,
-    highlight: function(str, lang) {
+    highlight: function(code_str, lang) {
       console.log('lang:',lang);
         if (lang && hljs.getLanguage(lang)) {
             try {
                 return '<pre class="hljs"><code>' +
-                    hljs.highlight(str, {
+                    hljs.highlight(code_str, {
                         language: lang,
                         ignoreIllegals: true
                     }).value +
                     '</code></pre>';
             } catch (__) {}
         }
-        else if(lang == 'mermaid'){
-            return mermaidChart(str)
+        if(lang == 'mermaid'){
+            return mermaidChart(code_str)
         }
-        return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+        if(lang == 'sequence'){
+          return sequenceChart(code_str)
+        }
+        return '<pre class="hljs"><code>' + md.utils.escapeHtml(code_str) + '</code></pre>';
     }
 });
 md.use(mk);
@@ -63,3 +71,41 @@ let article_url = './article/' + title;
 // article_url = './article/C代码编译过程.md';
 console.log('article_url:',article_url);
 render(article_url);
+
+
+$.fn.sequenceDiagram = function(options) {
+  return this.each(function() {
+    var $this = $(this);
+    var d = Diagram.parse($this.text());
+    $this.html('');
+    d.drawSVG(this, options);
+  });
+};
+
+window.onload = function () {
+  $(".diagram").sequenceDiagram();
+
+  /*
+  let code_str = `Title: 标题：复杂使用
+  对象A->对象B: 对象B你好吗?（请求）
+  Note right of 对象B: 对象B的描述
+  Note left of 对象A: 对象A的描述(提示)
+  对象B-->对象A: 我很好(响应)
+  对象B->小三: 你好吗
+  小三-->>对象A: 对象B找我了
+  对象A->对象B: 你真的好吗？
+  Note over 小三,对象B: 我们是朋友
+  participant C
+  Note right of C: 没人陪我玩`;
+  let svg = document.createElement('div');
+  svg.setAttribute('id','abcd');
+  let root = document.getElementById('content');
+  root.appendChild(svg);
+  let d = Diagram.parse(code_str);
+  console.log('d:',d);
+  d.drawSVG('abcd');
+  */
+};
+
+
+
