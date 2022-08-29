9a6150303f3188dc71a6f1d384048fc7de0e17e0
HeHuiqi <1710308677@qq.com>
2022-08-29 10:09:48 +0800

    fix bug

--- js/content.js
+++ js/content.js
@@ -1,28 +1,17 @@
 let url = window.location.href;
-// console.log('url:', url);
 let start = url.indexOf('=') + 1;
 let title = url.substring(start) + '.md';
 if (start == 0) {
    title = '基础语法.md'; 
 }
-
-// mermaid.initialize({startOnLoad:true});
+mermaid.initialize({startOnLoad:true});
 const mermaidChart = (code_str) => {
   try {
-    mermaid.parse(code_str)
     return `<div class="mermaid">${code_str}</div>`
   } catch ({ str, hash }) {
     return `<pre>${str}</pre>`
   }
 };
-const sequenceChart = (code_str) => {
-  try {
-    return `<div class="diagram">${code_str}</div>`
-  } catch ({ str, hash }) {
-    return `<pre>${str}</pre>`
-  }
-};
-
 const ct = document.getElementById('content');
 const mk = markdownitLatex2img;
 const md = new markdownit({
@@ -44,22 +33,16 @@ const md = new markdownit({
         if(lang == 'mermaid'){
             return mermaidChart(code_str)
         }
-        if(lang == 'sequence'){
-          return sequenceChart(code_str)
-        }
         return '<pre class="hljs"><code>' + md.utils.escapeHtml(code_str) + '</code></pre>';
     }
 });
 md.use(mk);
 
-
-
+mermaid.initialize({startOnLoad:true});
 
 function render(url) {
-    // console.log('render-url:', url);
     get(url, (mdc) => {
         const res = md.render(mdc);
-        // console.log('res:', res);
         ct.innerHTML = res;
     });
 }
@@ -73,39 +56,7 @@ console.log('article_url:',article_url);
 render(article_url);
 
 
-$.fn.sequenceDiagram = function(options) {
-  return this.each(function() {
-    var $this = $(this);
-    var d = Diagram.parse($this.text());
-    $this.html('');
-    d.drawSVG(this, options);
-  });
-};
 
-window.onload = function () {
-  $(".diagram").sequenceDiagram();
-
-  /*
-  let code_str = `Title: 标题：复杂使用
-  对象A->对象B: 对象B你好吗?（请求）
-  Note right of 对象B: 对象B的描述
-  Note left of 对象A: 对象A的描述(提示)
-  对象B-->对象A: 我很好(响应)
-  对象B->小三: 你好吗
-  小三-->>对象A: 对象B找我了
-  对象A->对象B: 你真的好吗？
-  Note over 小三,对象B: 我们是朋友
-  participant C
-  Note right of C: 没人陪我玩`;
-  let svg = document.createElement('div');
-  svg.setAttribute('id','abcd');
-  let root = document.getElementById('content');
-  root.appendChild(svg);
-  let d = Diagram.parse(code_str);
-  console.log('d:',d);
-  d.drawSVG('abcd');
-  */
-};
 
 
 
